461ffb25fe5e191bbe3e2b246aef7e6672c3ae9a
kategitt <kapitanEka@yandex.ru>
2023-01-15 12:45:13 +0300

    Решила задачу 1

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 6;
+
+int index = -N;
+
+while (index <= N)
+{
+    Console.WriteLine(index);
+    index = index + 1;
+}
